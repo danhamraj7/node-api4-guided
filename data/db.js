a178f5b470c6470ed07f38a0a60ff0fdcb7aeb70
danhamraj <danhamraj@gmail.com>
2020-03-20 23:41:48 -0400

    Added massage of the day

--- data/db.js
+++ data/db.js
@@ -1,6 +1,9 @@
 const knex = require("knex");
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.DB_ENV || "development";
+
 const config = require("../knexfile.js")[environment];
 
 module.exports = knex(config);
+
+//DB_ENV needed for heroku
